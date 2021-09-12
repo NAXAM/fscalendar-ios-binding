09ce0f95f0e69fad75f26dd742033f43abc9c359
Vu Duc Tuyen <tuyenvd@newwave.com.vn>
2021-09-12 21:02:59 +0700

    Bump v2.8.3

---
+++ Naxam.FSCalendar.iOS/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+using Foundation;
+
+// This attribute allows you to mark your assemblies as “safe to link”. 
+// When the attribute is present, the linker—if enabled—will process the assembly 
+// even if you’re using the “Link SDK assemblies only” option, which is the default for device builds.
+
+[assembly: LinkerSafe]
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("Naxam.FSCalendar.iOS")]
+[assembly: AssemblyDescription("Xamarin Binding Library - FSCalendar SDK Binding")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NAXAM COMPANY LIMITED")]
+[assembly: AssemblyProduct("X Bindings")]
+[assembly: AssemblyCopyright("Copyright (c) 2021 NAXAM")]
+[assembly: AssemblyTrademark("NAXAM")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("2.8.3")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
